257adfaea728998711eecf418cb1478ae79be600
VonC <vonc@laposte.net>
2022-01-23 12:25:59 +0100

    bat utilities: senv, build, run, all

---
+++ senv.bat
@@ -0,0 +1,9 @@
+@echo off
+doskey a=all.bat $*
+doskey b=build.bat $*
+doskey r=run.bat $*
+echo Environment Initialization done.
+echo Type:
+echo.- b for build
+echo.- r for run
+echo.- a for build+run
