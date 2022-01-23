5ac2ae1189842ca44c266dc7af6fd69a5094de21
VonC <vonc@laposte.net>
2022-01-23 19:10:58 +0100

    main.go: calls vscode.FindWorkspace(). empty list means none were found

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
 	"vsclauncher/logger"
@@ -8,11 +9,23 @@ import (
 )
 
 func main() {
-	logger.SetLevel("debug")
+	logger.SetLevel(logger.DEBUG)
 	logger.Debug("vsclauncher")
 	currentWorkingDirectory, err := os.Getwd()
 	if err != nil {
 		log.Fatal(err)
 	}
-	vscode.FindWorkspace(currentWorkingDirectory)
+	name := ""
+	if len(os.Args) > 1 {
+		name = os.Args[1]
+	}
+	w := vscode.FindWorkspace(currentWorkingDirectory, name)
+	logger.Debug("Main: workspace found: '%s'", w)
+	if w == "" {
+		m := ""
+		if name != "" {
+			m = " for name '" + name + "'"
+		}
+		fmt.Printf("No VSCode workspace found%s\n", m)
+	}
 }
