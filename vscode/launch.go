058e6d70f87904e6e47d6310ab1dc524dc4ffff9
VonC <vonc@laposte.net>
2024-10-08 15:51:20 +0200

    fix(vscode): launch.go look for VSCode into PRGS/vscodes/current

--- vscode/launch.go
+++ vscode/launch.go
@@ -14,19 +14,25 @@ func Launch(w string) {
 	logger.Debug("Launch '%s'\n", w)
 	d := os.Getenv("vscodei")
 	if d == "" {
-		d = filepath.Join(os.Getenv("LOCALAPPDATA"), "Programs\\Microsoft VS Code\\bin\\code.cmd")
+		d = filepath.Join(os.Getenv("LOCALAPPDATA"), "Programs", "Microsoft VS Code", "bin", "code.cmd")
 		if _, err := os.Stat(d); os.IsNotExist(err) {
-			d = filepath.Join(os.Getenv("ProgramFiles"), "Microsoft VS Code\\bin\\code.cmd")
+			d = filepath.Join(os.Getenv("ProgramFiles"), "Microsoft VS Code", "bin", "code.cmd")
+		}
+		if _, err := os.Stat(d); os.IsNotExist(err) {
+			d = filepath.Join(os.Getenv("PRGS"), "vscodes", "current", "bin", "code.cmd")
 		}
 	} else {
 		d = filepath.Join(d, "code.exe")
+	}
+	if _, err := os.Stat(d); os.IsNotExist(err) {
+		log.Fatalf("Unable to find VSCode executable '%s'", d)
 	}
 	// https://stackoverflow.com/questions/6376113/how-do-i-use-spaces-in-the-command-prompt
 	c := fmt.Sprintf(`"cd "%s" && "%s" "%s""`, ws.path, d, w)
 	//fmt.Println(c)
 
-	giterr, _, err := syscall.ExecCmd(c)
-	if err != nil || giterr.String() != "" {
-		log.Fatalf("Unable to launch VSCode workspace '%s': '%s' (%+v)", w, giterr.String(), err)
+	gitStdErr, _, err := syscall.ExecCmd(c)
+	if err != nil || gitStdErr.String() != "" {
+		log.Fatalf("Unable to launch VSCode workspace '%s': '%s' (%+v)", w, gitStdErr.String(), err)
 	}
 }
